dbcff71c2515cb60093cdf77c229a6e76ee32af9
Andrew Wentzel <tehwentzel@gmail.com>
2019-04-03 09:09:44 -0500

    sidebar basic functionality working

---
+++ controller.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	console.log('loaded');
+	
+	$('.accountIcon').on('click', function(){
+		if(this.id == 'plusIcon'){
+			return;
+		}
+		var selected = this;
+		var topPos = $('.accountIcon:first-child').css('top');
+		console.log(top);
+		$('.accountIcon').each(function(){
+			if(this !==  selected){
+				var toClear = this;
+				$(this).animate({
+					opacity: 0
+				}, 200, function(){
+						$(toClear).css('display', 'none');
+				});
+			}
+		});
+		$('.accountInfo').css('display', 'flex');
+		$(this).css('cursor', 'default');
+		$('#backArrowLeft').css({
+			visibility: 'visible',
+			cursor: 'pointer'
+		});
+	});
+	
+	$('#backArrowLeft').on('click', function(){
+		$('.accountInfo').css('display', 'none');
+		$('.accountIcon').animate({
+			opacity: 1,
+		}, 200, function(){
+			$(this).css('display', 'flex');
+			$(this).css('cursor', 'pointer');
+		})
+		$('#backArrowLeft').css({
+			visibility: 'hidden',
+			cursor: 'default'
+		});
+	});
+});
