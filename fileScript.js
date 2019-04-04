38de2f8129974d398c68e66940015b3d0d6a0eae
Andrew Wentzel <tehwentzel@gmail.com>
2019-04-04 17:56:37 -0500

    incorporated files

---
+++ fileScript.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	// get the modal
+	var modal = document.getElementById('fileModal');
+	// get the span element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// when the user clicks on the button, open the modal
+	$('.fileImage').on('click', function(){
+		console.log('click');
+		console.log(modal);
+		document.getElementById('fileModal').style.display = "block";
+	});
+
+	// when the user clicks on the span X, close the modal
+	span.onclick = function()
+	{
+		document.getElementById('fileModal').style.display = "none";
+	}
+
+	// when the user clicks anywhere outside it, close it
+	$('.file-modal-class').on('click', function(ev)
+	{	
+		if (ev.target.classList.contains('.modal-content-class'))
+		{
+			ev.stopPropagation();
+		}else{
+			modal.style.display = "none";
+		}
+	});
+
+	// download button inside modal
+	$('#downloadButton').on('click', function()
+	{
+		var dlButton = document.getElementById('downloadButton');
+		
+		if (dlButton.value == "Download"){
+			dlButton.value = "Downloading...";
+		}
+		else if (dlButton.value == "Downloading...")
+			dlButton.value = "Already Downloading...";
+		setTimeout(function(){
+			dlButton.value = "Downloaded";
+		}, 2000);
+	});
+
+	$('#sendButton').on('click', function()
+	{
+		var sendButton = document.getElementById('sendButton');
+		
+		if (sendButton.value == "Send")
+			sendButton.value = "Sent!";
+		else
+			sendButton.value = "Send";
+	});
+})
