1b5ffeb4d8d90c9aef524a2780d976e9b4134dc0
MichaelJHoang <43363847+MichaelJHoang@users.noreply.github.com>
2019-04-23 20:37:36 -0500

    Add files via upload
    refined files section

--- fileScript.js
+++ fileScript.js
@@ -1,50 +1,25 @@
 $(document).ready(function() {
 	// get the modal
 	var modal = document.getElementById('fileModal');
-	// get the span element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
 	// variable to keep track of the index of the sampleDocs
 	var index = 1;
-	
+	$('#fileModal').on('click', function(){
+		this.style.display = 'none';
+	});
+	$('.modal-content-class').on('click', function(ev){
+		ev.stopPropagation();
+		ev.preventDefault();
+	});
 	// when the user clicks on the button, open the modal
 	$('.fileImage, .feedFile').on('click', function()
 	{
-		// check to see what specific file they clicked and open set the index
-		// corresponding to the file number
-		if (this.src.indexOf ("images/sampleDoc1.png") != -1)
-		{
-			index = 1;
-		}
-		else if (this.src.indexOf ("images/sampleDoc2.png") != -1)
-		{
-			index = 2;
-		}
-		else if (this.src.indexOf ("images/sampleDoc3.png") != -1)
-		{
-			index = 3;
-		}
-		else if (this.src.indexOf ("images/sampleDoc4.png") != -1)
-		{
-			index = 4;
-		}
-		else if (this.src.indexOf ("images/sampleDoc5.png") != -1)
-		{
-			index = 5;
-		}
-		
 		// append the strings and replace the default src so that the user can actually
 		// view the thing that they've clicked
-		$(".fileViewer").attr("src", "images/sampleDoc" + index + ".png");
+		$(".fileViewer").attr("src",this.src);
 		document.getElementById('fileModal').style.display = "block";
 		
 		return false;
 	});
-
-	// when the user clicks on the span X, close the modal
-	span.onclick = function()
-	{
-		document.getElementById('fileModal').style.display = "none";
-	}
 
 	// download button inside modal
 	$('#downloadButton').on('click', function()
@@ -70,4 +45,13 @@ $(document).ready(function() {
 		else
 			sendButton.value = "Send";
 	});
+	
+	// encapsulate the file images with alt wrap so that the hover effect can work
+	$(".fileImage").wrap('<div class="alt-wrap"/>');
+
+	// used for file image hover effect
+	$(".fileImage").each(function() 
+	{
+		$(this).after('<p class="alt">' + $(this).attr('alt') + '</p>');
+	})
 })
